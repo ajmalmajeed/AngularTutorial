ad9480cc0572bac2e635d19c24e4c48b1c032375
ajmalmajeed <32757909+ajmalmajeed@users.noreply.github.com>
2018-06-21 11:00:31 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+   
   ],
   imports: [
     BrowserModule,
