02517af336f62313b9b689731de43b3f9ee9cb7e
Ajmal <majmalamajeed@gmail.com>
2018-06-21 16:15:02 +0530

    adding routes, routerModule for root

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes, Router } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent}
+]
 
 @NgModule({
-  imports: [
+  exports: [
+    RouterModule
     
   ],
+  
+    imports: [
+      RouterModule.forRoot(routes)
+      
+    ],
 
 })
 export class AppRoutingModule { }
